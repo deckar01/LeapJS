38d52e54b1f890618e2e621d9de88854f469ca9a
Jared Deckard <deckar01@nsuok.edu>
2013-02-04 23:00:20 -0600

    Added AnimLoop and fixed official visualizer

--- Leap.js
+++ Leap.js
@@ -1,5 +1,38 @@
-var Leap = { APIVersion : "0.7.2" };
+var Leap = { APIVersion : "0.7.3" };
 
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame    ||
+    window.oRequestAnimationFrame      ||
+    window.msRequestAnimationFrame     ||
+    function( callback ){
+        window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+Leap.AnimLoop = function(controller, callback){
+	
+	this._controller = controller;
+	this._callback = callback;
+	
+	var me = this;
+	
+	this._loop = function(controller){
+		window.requestAnimFrame(function(){ me._loop(me._controller); });
+		me._callback(controller);
+	};
+	
+	if(controller.isConnected()) window.requestAnimFrame(function(){ me._loop(me._controller); });
+	else{
+		this._listener = new Leap.Listener();
+		this._listener.onConnect = function(controller){
+			me._controller.removeListener(me._listener);
+			window.requestAnimFrame(function(){ me._loop(me._controller); });
+		};
+		controller.addListener(this._listener);
+	}
+};
 Leap.Calibrate = function(controller){
 	
 	this._controller = controller;
