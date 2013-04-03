75f7ebb0ccc1e79a1d2a0891a5482cc8fa529fd7
Jared Deckard <deckar01@nsuok.edu>
2013-04-03 00:30:25 -0500

    Reused Pointable prototype

--- Leap.js
+++ Leap.js
@@ -1197,34 +1197,18 @@ Leap.PointableList.prototype._delete = function(){
 
 Leap.FingerList = function(){};
 
-Leap.FingerList.prototype = new Array;
+Leap.FingerList.prototype = Leap.PointableList.prototype;
 
 Leap.FingerList.prototype.append = function(other){
 	for(i = 0; i < other.length; i++) this.push(new Leap.Finger(other[i]));
 };
 
-Leap.FingerList.prototype.count = function(){
-	return this.length;
-};
-
-Leap.FingerList.prototype.empty = function(){
-	return this.length > 0;
-};
-
 Leap.ToolList = function(){};
 
-Leap.ToolList.prototype = new Array;
+Leap.ToolList.prototype = Leap.PointableList.prototype;
 
 Leap.ToolList.prototype.append = function(other){
 	for(i=0; i<other.length; i++) this.push(new Leap.Tool(other[i]));
-};
-
-Leap.ToolList.prototype.count = function(){
-	return this.length;
-};
-
-Leap.ToolList.prototype.empty = function(){
-	return this.length>0;
 };
 
 Leap.Screen = function(data){
